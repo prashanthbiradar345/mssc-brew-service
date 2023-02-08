cf2d92bdb55a8c8045687917c21adc4b0743d384
Prashanth <Prashanthbiradar345@gmail.com>
2023-02-08 20:11:53 +0530

    Mscc Beer Service Project added

---
+++ src/main/java/com/biradar/msscbrewservice/MsscBrewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.biradar.msscbrewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBrewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBrewServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/biradar/msscbrewservice/MsscBrewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biradar.msscbrewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBrewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
